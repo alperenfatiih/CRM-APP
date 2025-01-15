70a5239ce057d3a7377febdbc587216197435293
alperen fatih karataş <105991154+alperenfatiih@users.noreply.github.com>
2025-01-15 14:03:14 +0300

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXDu4BkGgwkNpoyP8Zm-c34pI8dgPpZ-w",
-  authDomain: "crmapp-57303.firebaseapp.com",
-  projectId: "crmapp-57303",
-  storageBucket: "crmapp-57303.firebasestorage.app",
-  messagingSenderId: "68988626926",
-  appId: "1:68988626926:web:80d1862634a6ee6e9e10ff"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
